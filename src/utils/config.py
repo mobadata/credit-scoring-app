9c274130f0ad0492a17fb6634d83a519997b8445
Moussa Ba <moussaba@MacBook-Pro-de-Moussa.local>
2025-07-19 01:27:48 +0200

    Ajout initial du projet

---
+++ src/utils/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Chemin racine du projet
+BASE_DIR = "/Users/moussaba/Desktop/Moussa_BA/credit-scoring-app"
+
+# Dossier data
+DATA_PATH = os.path.join(BASE_DIR, "data")
+
+# Fichier CSV compressé
+TRAIN_ZIP_FILE = os.path.join(DATA_PATH, "application_train.csv.zip")
